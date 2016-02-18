37280581953282afe7d3d9b517663a95be84817d
Sami Koho <sami.koho@gmail.com>
2016-02-18 17:04:40 +0200

    Started to work on the new HDF5 file structure. It starts to be ready for testing, but potentially full of typos, of course.

--- supertomo/bin/convert_files.py
+++ supertomo/bin/convert_files.py
@@ -1,2 +1,14 @@
-__author__ = 'Sami Koho'
-__email__ = 'sami.koho@gmail.com'
+"""
+A program to convert image files into our HDF5 archive format
+"""
+
+def main():
+    pass
+
+# Read source images
+
+# Save into HDF5 format
+
+
+if __name__ == "__main__":
+    main()
