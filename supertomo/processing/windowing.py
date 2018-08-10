4c59b35bfac0a0fe02ad82642d7f2cc6dc0f1a15
Sami Koho <sami.koho@gmail.com>
2018-08-10 15:18:11 +0200

    A lot fo Notebooks. I will have to move them somewhere else, except for a couple of examples. Made a sectioned FRC iterator as well.

--- supertomo/processing/windowing.py
+++ supertomo/processing/windowing.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def _nd_window(data, filter_function):
     """
-    Performs an in-place windowing on N-dimensional spatial-domain data.
+    Performs on N-dimensional spatial-domain data.
     This is done to mitigate boundary effects in the FFT.
 
     Parameters
@@ -15,6 +15,7 @@ def _nd_window(data, filter_function):
            Function should accept one argument: the window length.
            Example: scipy.signal.hamming
     """
+    result = data.copy().astype(np.float64)
     for axis, axis_size in enumerate(data.shape):
         # set up shape for numpy broadcasting
         filter_shape = [1, ] * data.ndim
@@ -22,7 +23,8 @@ def _nd_window(data, filter_function):
         window = filter_function(axis_size).reshape(filter_shape)
         # scale the window intensities to maintain image intensity
         np.power(window, (1.0/data.ndim), out=window)
-        return data * window
+        result *= window
+    return result
 
 
 def apply_hamming_window(data):
