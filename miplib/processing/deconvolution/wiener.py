d90ec77b05eea8e83def6f7c5763456dc0f84402
Sami Koho <sami.koho@gmail.com>
2018-08-27 14:02:43 +0200

    Renamed the library to MIPLIB. Added a basic conda package description.

---
+++ miplib/processing/deconvolution/wiener.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from numpy.fft import fftn, ifftn, fftshift
+
+from miplib.data.containers.image import Image
+import miplib.processing.image as imops
+
+#todo: Speed up with CUDA/Multithreading. Functions are ready in the ufuncs.py
+
+def wiener_deconvolution(image, psf, snr=1e-3, add_pad=0, cuda=False):
+    assert isinstance(image, Image)
+    assert isinstance(psf, Image)
+
+    image_s = Image(image.copy(), image.spacing)
+
+    if image.ndim != psf.ndim:
+        raise ValueError("Image and psf dimensions do not match")
+
+    if psf.spacing != image.spacing:
+        psf = imops.zoom_to_spacing(psf, image.spacing)
+
+    if add_pad != 0:
+        new_shape = list(i + 2*add_pad for i in image_s.shape)
+        image_s = imops.zero_pad_to_shape(image_s, new_shape)
+
+    if psf.shape != image_s.shape:
+        psf = imops.zero_pad_to_shape(psf, image_s.shape)
+
+    psf /= psf.max()
+
+    psf_f = fftn(fftshift(psf))
+
+    wiener = (np.abs(psf_f)**2/(np.abs(psf_f)**2 + snr))/psf_f
+
+    image_f = fftn(image_s)
+
+    result = Image(np.abs(ifftn(image_f * wiener).real), image.spacing)
+
+    return imops.remove_zero_padding(result, image.shape)
+
+
---
+++ miplib/processing/registration/observers.py
@@ -0,0 +1,63 @@
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+from ipywidgets import interact
+
+from miplib.ui.plots import image
+from miplib.processing import itk as itkutils
+
+
+def start_observer():
+    """
+    Start the registration observer. A list is initialized for the metric
+    values
+    """
+    global metric_values
+
+    metric_values = []
+
+
+def update_observer(registration_method):
+    """
+    Adds the current metric value to the list.
+    """
+    global metric_values
+
+    metric_values.append(registration_method.GetMetricValue())
+
+
+def plot_result(self, fixed, moving):
+    """
+    Show registration results either in a Jupyter notebook, or Fiji.
+
+    Parameters
+    ----------
+    fixed   The fixed image
+    """
+    global metric_values
+
+    result = itkutils.resample_image(moving,
+                                     self.transform,
+                                     reference=fixed)
+
+    plt.subplots(1, 2, figsize=(10, 8))
+    fixed = itkutils.convert_from_itk_image(sitk.Cast(fixed, sitk.sitkUInt8))
+    moving = itkutils.convert_from_itk_image(
+        sitk.Cast(result, sitk.sitkUInt8)
+    )
+
+    def update(layer):
+        # Plot metric values
+        plt.subplot(1, 2, 1)
+        plt.plot(metric_values, 'r')
+        plt.title("Metric values")
+        plt.xlabel('Iteration Number', fontsize=12)
+        plt.ylabel('Metric Value', fontsize=12)
+
+        # Plot image overlay
+        plt.subplot(1, 2, 2)
+        plt.title("Overlay")
+        image.display_2d_image_overlay(fixed[layer, :, :], moving[layer, :, :])
+
+    interact(update, layer=(0, fixed.shape[0] - 1, 1))
+
+    del metric_values
