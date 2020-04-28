8b02809b7ce699aca30566c523613c9203678af8
Sami Koho <3784623+sakoho81@users.noreply.github.com>
2020-04-28 11:09:00 +0200

    Added Zenodo badge. Some reorganization.

--- miplib/data/coordinates/polar.py
+++ miplib/data/coordinates/polar.py
@@ -8,6 +8,19 @@ indexing structures (masks).
 import numpy as np
 
 
+def generate_polar_coordinate_grid(shape, spacing):
+    """
+    Generate a scaled polar coordinate grid axes.
+    :param shape: the size of the grid
+    :param spacing: the spacing between grid elements
+    :return: the generated coordinate axes (tuple)
+    """
+
+    axes = tuple(np.arange(-shape_n // 2, shape_n // 2, 1) * spacing_n
+                 for shape_n, spacing_n in zip(shape, spacing))
+    return axes
+
+
 class SimplePolarIndexer(object):
     """
     Basic indexer for a polar/spherical coordinate system
