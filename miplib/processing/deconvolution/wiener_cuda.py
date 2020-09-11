6277f8ffaa0f275d1534d987040deeef8e979481
Sami Koho <sami.koho@outlook.com>
2020-09-11 19:35:34 +0200

    A working CUDA Wiener filter

--- miplib/processing/deconvolution/wiener_cuda.py
+++ miplib/processing/deconvolution/wiener_cuda.py
@@ -9,6 +9,9 @@ import miplib.processing.ndarray as arrayops
 
 
 def wiener_deconvolution(image, psf, snr=30, add_pad=0):
+    """ A GPU accelerated implementation of a linear Wiener filter. Some effort is made
+    to allow processing even relatively large images, but some kind of block-based processing
+     (as in the RL implementation) may be required in some cases."""
     assert isinstance(image, Image)
     assert isinstance(psf, Image)
 
@@ -22,7 +25,7 @@ def wiener_deconvolution(image, psf, snr=30, add_pad=0):
         psf = imops.zoom_to_spacing(psf, image.spacing)
 
     if add_pad != 0:
-        new_shape = list(i + 2*add_pad for i in image_s.shape)
+        new_shape = list(i + 2 * add_pad for i in image_s.shape)
         image_s = imops.zero_pad_to_shape(image_s, new_shape)
 
     if psf.shape != image_s.shape:
@@ -31,28 +34,27 @@ def wiener_deconvolution(image, psf, snr=30, add_pad=0):
     psf /= psf.max()
     psf = fftshift(psf)
 
-    psf_dev = fftn(cp.asarray(psf), overwrite_x=True)
+    psf_dev = cp.asarray(psf.astype(np.complex64))
+    with get_fft_plan(psf_dev):
+        psf_dev = fftn(psf_dev, overwrite_x=True)
 
-    stream = cp.cuda.stream.Stream()
-    with stream:
-        psf_abs = cp.abs(psf_dev)**2
-        below = cp.asnumpy(psf_dev)
-    stream.synchronize()
-
-    with stream:
-        psf_abs /= (psf_abs + snr)
-        image_dev = fftn(cp.asarray(image_s), overwrite_x=True)
-    stream.synchronize()
-
+    below = cp.asnumpy(psf_dev)
+    psf_abs = cp.abs(psf_dev) ** 2
+    psf_abs /= (psf_abs + snr)
     above = cp.asnumpy(psf_abs)
-    wiener = arrayops.safe_divide(above, below)
+    psf_abs = None
+    psf_dev = None
 
-    psf_dev = cp.asarray(wiener)
-    image_dev *= psf_dev
+    image_dev = cp.asarray(image_s.astype(np.complex64))
+    with get_fft_plan(image_dev):
+        image_dev = fftn(image_dev, overwrite_x=True)
 
-    result = np.abs(cp.asnumpy(ifftn(image_dev, overwrite_x=True)).real)
+    wiener_dev = cp.asarray(arrayops.safe_divide(above, below))
+
+    image_dev *= wiener_dev
+
+    result = cp.asnumpy(cp.abs(ifftn(image_dev, overwrite_x=True)).real)
     result = Image(result, image.spacing)
 
     return imops.remove_zero_padding(result, orig_shape)
-
 
